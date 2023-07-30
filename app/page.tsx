cfc1101847b592b1d497a1816592fd24bcfd27e3
kamalika0363 <59131714+doesnots@users.noreply.github.com>
2023-07-30 21:35:15 +0530

    opacity

--- app/page.tsx
+++ app/page.tsx
@@ -12,35 +12,35 @@ const movies: Movie[] = [
     title: "Spider-Man: Across the Universe",
     rating: 4.7,
     image: "spider-man.svg",
-    color: "#3D4763",
+    color: "rgba(61, 71, 99, 0.6)",
   },
   {
     title: "Barbie: Reinventing the Dream",
     rating: 4.5,
     image: "barbie.svg",
-    color: "#FD9DD1",
+    color: "rgba(253, 157, 209, 0.6)",
   },
   {
     title: "Mission: Impossible - Dead Reckoning",
     rating: 4.5,
     image: "missionimpossible.svg",
-    color: "#443632",
+    color: "rgba(68, 54, 50, 0.6)",
   },
 ];
 
 export default function Home() {
   return (
     <>
-      <h1 className="text-2xl font-bold mb-2 pl-5 pt-5 md:text-left md:pl-80 lg:pl-80">
+      <h1 className="text-2xl font-bold mb-2 pl-5 pt-5 md:text-left md:pl-0 lg:pl-80">
         Popular Movies 2023
       </h1>
       <div className="flex flex-col items-end">
         <div className="flex flex-wrap justify-center">
           {movies.map((movie) => (
             <div key={movie.title} className="flex flex-col items-center mx-4 my-2 relative">
-              <img src={movie.image} alt={movie.title} className="h-72 w-96 rounded-lg" />
+              <img src={movie.image} alt={movie.title} className="h-72 w-96 rounded-xl" />
               <div
-                className="absolute bottom-0 bg-opacity-60 w-full rounded-xl p-2"
+                className="absolute bottom-12 md:w-full rounded-3xl p-2 md:bottom-6 w-80"
                 style={{ backgroundColor: movie.color }}
               >
                 <h2 className="text-lg font-bold text-white">{movie.title}</h2>
