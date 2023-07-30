342d1991d4c35993f1b1d0de1b329c418e1c49c7
kamalika0363 <59131714+doesnots@users.noreply.github.com>
2023-07-30 21:11:28 +0530

    popular movies ✅

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+// import Navbar from '../components/Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      {/* <Navbar /> */}
       <body className={inter.className}>{children}</body>
     </html>
   )
