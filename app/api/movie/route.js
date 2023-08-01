4b2d9984faad21fc270a897ba3c7514c84afee1f
Dhruv <dhruvrg2003@gmail.com>
2023-08-01 19:12:29 +0530

    merge 3 tables in one

--- app/api/movie/route.js
+++ app/api/movie/route.js
@@ -4,7 +4,7 @@ import { connectToDB } from "../../../utils/database";
 export const GET = async () => {
   try {
     await connectToDB();
-    const movies = await Movies.find({});
+    const movies = await Movies.find({ 'rating' : { $exists: true, $ne: null } });
     return new Response(JSON.stringify(movies), { status: 200 });
   } catch (error) {
     return new Response("Failed to fetch all", { status: 500 });
--- app/api/watchnowmovies/route.js
+++ app/api/watchnowmovies/route.js
@@ -1,10 +1,10 @@
-import Watchnowmovies from "../../../models/watchnowmovies";
+import Movies from "../../../models/movies";
 import { connectToDB } from "../../../utils/database";
 
 export const GET = async () => {
   try {
     await connectToDB();
-    const movies = await Watchnowmovies.find({});
+    const movies = await Movies.find({$and:[{ 'time_remaining' : { $exists: false } },{ 'rating' : { $exists: false } }]});
     return new Response(JSON.stringify(movies), { status: 200 });
   } catch (error) {
     return new Response("Failed to fetch all", { status: 500 });
